6b125fb92fc124c53b54b9c00af39821dee9ca13
Mateusz Fiślak <fislak.mateusz@gmail.com>
2016-05-06 10:47:57 +0200

    Category expand works

--- app/src/main/java/com/example/win7/restapitest/screens/restaurant_menu_screen/MenuParentViewHolder.java
+++ app/src/main/java/com/example/win7/restapitest/screens/restaurant_menu_screen/MenuParentViewHolder.java
@@ -2,6 +2,7 @@ package com.example.win7.restapitest.screens.restaurant_menu_screen;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.bignerdranch.expandablerecyclerview.ViewHolder.ParentViewHolder;
@@ -13,25 +14,30 @@ import com.example.win7.restapitest.R;
 public class MenuParentViewHolder extends ParentViewHolder {
 
     public TextView categoryName;
+    public Button button;
 
     public MenuParentViewHolder(View itemView) {
         super(itemView);
 
         categoryName = (TextView) itemView.findViewById(R.id.category_name);
+        button = (Button) itemView.findViewById(R.id.expand_button);
 
-
-
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isExpanded()) {
+                    collapseView();
+                } else {
+                    expandView();
+                }
+            }
+        });
     }
+
 
     @Override
-    public void onClick(View v) {
-        if (isExpanded()) {
-            collapseView();
-        } else {
-            expandView();
-        }
+    public boolean shouldItemViewClickToggleExpansion() {
+        return false;
     }
-
-
 
 }
